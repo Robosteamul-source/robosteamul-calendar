6df5c81fd98245983618da462aae3646b18dee5f
Igor Shlein <info@robosteamul.com>
2026-06-02 07:53:47 +0500

    Добавил личный кабинет родителя

---
+++ repos/MyProjectWeb/MyProjectWeb/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyProjectWeb
+{
+    class MainClass { 
+        static void Main(string[] args) { 
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
